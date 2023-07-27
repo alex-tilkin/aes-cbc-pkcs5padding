2f24a1f75e64c59ce6bf49a6b7aa8e7abe27c1ab
Alexander Tilkin <alextilk@gmail.com>
2023-07-27 15:22:47 +0300

    Adding all the files to the repo

---
+++ src/main/java/com/example/aesdemo/AesDemoApplication.java
@@ -0,0 +1,82 @@
+package com.example.aesdemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+@SpringBootApplication
+public class AesDemoApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AesDemoApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+//        SecretKey secretKeyOrig = generateKey(256); // Generate a secret key
+//        String encryptionKey = Base64.getEncoder().encodeToString(secretKeyOrig.getEncoded()); // convert the secret key to string
+        String encryptionKey = "cWrkCbX1JKCiWYFDx9DsHKqdn38QK5o3";
+        SecretKey secretKeyFromStr = convertStringToSecretKeyto(encryptionKey); // Convert the string to secret key
+        IvParameterSpec ivParameterSpec = generateIv(); // Generate Initialization Vector
+        String input = "Complyt";
+        String algorithm = "AES/CBC/PKCS5Padding";
+
+        String cipherText = encrypt(algorithm, input, secretKeyFromStr, ivParameterSpec);
+        String plainText = decrypt(algorithm, cipherText, secretKeyFromStr, ivParameterSpec);
+        System.out.println(cipherText + "; " + plainText);
+    }
+
+    public static SecretKey convertStringToSecretKeyto(String encodedKey) {
+        byte[] decodedKey = Base64.getDecoder().decode(encodedKey);
+        SecretKey originalKey = new SecretKeySpec(decodedKey, 0, decodedKey.length, "AES");
+
+        return originalKey;
+    }
+
+    public static SecretKey generateKey(int n) throws NoSuchAlgorithmException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(n);
+        SecretKey key = keyGenerator.generateKey();
+
+        return key;
+    }
+
+    public static IvParameterSpec generateIv() {
+        byte[] iv = new byte[16];
+        new SecureRandom().nextBytes(iv);
+
+        return new IvParameterSpec(iv);
+    }
+
+    public static String encrypt(String algorithm, String input, SecretKey key, IvParameterSpec iv)
+            throws NoSuchPaddingException, NoSuchAlgorithmException,
+            InvalidAlgorithmParameterException, InvalidKeyException,
+            BadPaddingException, IllegalBlockSizeException {
+        Cipher cipher = Cipher.getInstance(algorithm);
+        cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+        byte[] cipherText = cipher.doFinal(input.getBytes());
+
+        return Base64.getEncoder().encodeToString(cipherText);
+    }
+
+    public static String decrypt(String algorithm, String cipherText, SecretKey key, IvParameterSpec iv)
+            throws NoSuchPaddingException, NoSuchAlgorithmException,
+            InvalidAlgorithmParameterException, InvalidKeyException,
+            BadPaddingException, IllegalBlockSizeException {
+        Cipher cipher = Cipher.getInstance(algorithm);
+        cipher.init(Cipher.DECRYPT_MODE, key, iv);
+        byte[] plainText = cipher.doFinal(Base64.getDecoder().decode(cipherText));
+
+        return new String(plainText);
+    }
+
+}
---
+++ src/test/java/com/example/aesdemo/AesDemoApplicationTests.java
@@ -0,0 +1,61 @@
+package com.example.aesdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class AesDemoApplicationTests {
+
+    @Test
+    void genereate_key_test() throws NoSuchAlgorithmException {
+        System.out.println(Base64.getEncoder().encodeToString(AesDemoApplication.generateKey(256).getEncoded()));
+    }
+    @Test
+    void encrypt_decrypt_identical_input_equals_plain_text() throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException,
+            NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+        String secretKeyStr = "cWrkCbX1JKCiWYFDx9DsHKqdn38QK5o3";
+        SecretKey secretKey = AesDemoApplication.convertStringToSecretKeyto(secretKeyStr); // Convert the string to secret key
+        IvParameterSpec ivParameterSpec = AesDemoApplication.generateIv(); // Generate Initialization Vector
+        String encodedIv = Base64.getEncoder().encodeToString(ivParameterSpec.getIV());
+        String input = "Complyt";
+        String algorithm = "AES/CBC/PKCS5Padding";
+
+        String cipherText = AesDemoApplication.encrypt(algorithm, input, secretKey, ivParameterSpec);
+        String plainText = AesDemoApplication.decrypt(algorithm, cipherText, secretKey, ivParameterSpec);
+        System.out.println("Input: " + input + "; IV: " + encodedIv + "; Cipher text: " + cipherText + "; Plaint text: " + plainText);
+
+        assertEquals(input, plainText);
+    }
+
+    @Test
+    void encrypt_decrypt_extract_iv_and_generate_fromstring_inout_equals_plain_text() throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException,
+            NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+        String secretKeyStr = "cWrkCbX1JKCiWYFDx9DsHKqdn38QK5o3";
+        SecretKey secretKey = AesDemoApplication.convertStringToSecretKeyto(secretKeyStr); // Convert the string to secret key
+        IvParameterSpec ivParameterSpec = AesDemoApplication.generateIv(); // Generate Initialization Vector
+        String encodedIv = Base64.getEncoder().encodeToString(ivParameterSpec.getIV());
+
+        IvParameterSpec ivParameterSpecFromIv = new IvParameterSpec(Base64.getDecoder().decode(encodedIv));
+        String input = "This is an input text";
+        String algorithm = "AES/CBC/PKCS5Padding";
+
+        String cipherText = AesDemoApplication.encrypt(algorithm, input, secretKey, ivParameterSpec);
+        System.out.println(cipherText);
+        String plainText = AesDemoApplication.decrypt(algorithm, cipherText, secretKey, ivParameterSpecFromIv);
+        System.out.println("Input: " + input + "; IV: " + encodedIv + "; Cipher text: " + cipherText + "; Plain text: " + plainText);
+
+        assertEquals(input, plainText);
+    }
+}
